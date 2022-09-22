a0c71bdc675f88149c9e2fe876f5954edda2071a
angelica <angelica.cavero@gmail.com>
2022-09-23 00:38:12 +0200

    react responsive added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -35,68 +35,70 @@ function NavBar() {
             <img src={logo} className="App-logo" alt="logo" /> 
         </Link>
         <nav className='links-wrapper'>   
-            <div className='menu-links'>
-                <button className='close-button' onClick={handleMenuIcon}>
-                    <img src={ menuIcon ? iconMenu : iconClose} alt="close icon" />
-                </button>    
-                <div className='main-links'>
-                    <div className='link-arrow-wrapper'>
-                        <Link className='links' to="/" onClick={handleFeatureLink}>
-                            Features <img className='link-icon' src={iconFeatureToggle ? iconArrowUp : iconArrowDown}/>     
-                        </Link> 
-                        { iconFeatureToggle &&
-                                <div className='feature-options'>
-                                    <ul className='list-option'>
-                                        <li>
-                                            <img className='list-icon' src={iconTodo} alt="icon todo" /> 
-                                            <span>Todo List</span> 
-                                        </li>
-                                        <li>
-                                            <img className='list-icon' src={iconCalendar} alt="icon todo" /> 
-                                            <span>Calendar</span>
-                                        </li>
-                                        <li>
-                                            <img className='list-icon' src={iconReminders} alt="icon todo" /> 
-                                            <span>Reminders</span>
-                                        </li>
-                                        <li>
-                                            <img className='list-icon' src={iconPlanning} alt="icon todo" /> 
-                                            <span>Planning</span>
-                                        </li>
-                                    </ul>
-                                </div>
-                            }
-                    </div>
-                    <div className='link-arrow-wrapper'>
-                        <Link className='links' to="/" onClick={handleCompanyLink}>
-                            Company <img className='link-icon' src={iconCompanyToggle ? iconArrowUp : iconArrowDown}/>   
-                        </Link>
-                        { iconCompanyToggle &&
-                            <div className='company-options'>
-                                <ul className='list-option'>
-                                    <li>History</li>
-                                    <li>Our Team</li>
-                                    <li>Blog</li>
-                                </ul>
+            <button className='close-button' onClick={handleMenuIcon}>
+                <img src={ menuIcon ? iconMenu : iconClose} alt="close icon" />
+            </button> 
+                <div className='all-links'>
+                    <div className='menu-links'>           
+                        <div className='main-links'>
+                            <div className='link-arrow-wrapper'>
+                                <Link className='links' to="/" onClick={handleFeatureLink}>
+                                    Features <img className='link-icon' src={iconFeatureToggle ? iconArrowUp : iconArrowDown}/>     
+                                </Link> 
+                                { iconFeatureToggle &&
+                                        <div className='feature-options'>
+                                            <ul className='list-option'>
+                                                <li>
+                                                    <img className='list-icon' src={iconTodo} alt="icon todo" /> 
+                                                    <span>Todo List</span> 
+                                                </li>
+                                                <li>
+                                                    <img className='list-icon' src={iconCalendar} alt="icon todo" /> 
+                                                    <span>Calendar</span>
+                                                </li>
+                                                <li>
+                                                    <img className='list-icon' src={iconReminders} alt="icon todo" /> 
+                                                    <span>Reminders</span>
+                                                </li>
+                                                <li>
+                                                    <img className='list-icon' src={iconPlanning} alt="icon todo" /> 
+                                                    <span>Planning</span>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    }
                             </div>
-                        }
+                            <div className='link-arrow-wrapper'>
+                                <Link className='links' to="/" onClick={handleCompanyLink}>
+                                    Company <img className='link-icon' src={iconCompanyToggle ? iconArrowUp : iconArrowDown}/>   
+                                </Link>
+                                { iconCompanyToggle &&
+                                    <div className='company-options'>
+                                        <ul className='list-option'>
+                                            <li>History</li>
+                                            <li>Our Team</li>
+                                            <li>Blog</li>
+                                        </ul>
+                                    </div>
+                                }
+                            </div>
+                            <div>
+                                <Link className='links' to="/">Careers</Link> 
+                            </div>
+                            <div>
+                                <Link className='links' to="/">About</Link>          
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <Link className='links' to="/">Careers</Link> 
-                    </div>
-                    <div>
-                        <Link className='links' to="/">About</Link>          
+                    <div className='menu-login'>
+                        <div>
+                            <Link className='links' to="/">Login</Link>
+                        </div>
+                        <div>
+                            <button className='register-button'>Register</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='menu-login'>
-                <div>
-                    <Link className='links' to="/">Login</Link>
-                </div>
-                <div>
-                    <button className='register-button'>Register</button>
-                </div>
-            </div>   
         </nav>
       </header>
   )
--- src/containers/MainPage.jsx
+++ src/containers/MainPage.jsx
@@ -6,31 +6,36 @@ import clientDatabiz from '../assets/images/client-databiz.svg';
 import clientAudiophile from '../assets/images/client-audiophile.svg';
 import clientMeet from '../assets/images/client-meet.svg';
 import clientMarker from '../assets/images/client-maker.svg';
+import { useMediaQuery } from 'react-responsive'
 
 function MainPage() {
-  return (
-    <>  
-        <div className='main-page-container'>
-            <div className='main-page-text'>
-                <h1>Make remote work</h1>
-                <p>Get your team in sync, no matter your location. <br /><span>Streamline processes. create team rituals, and watch productivity soar.</span></p>
-                
-                <button className='learn-more-button'>
-                    Learn more
-                </button>
-                <div>
-                    <div className='client-image-wrapper'>
-                        <img className='client-image' src={clientDatabiz}/>
-                        <img className='client-image' src={clientAudiophile}/>
-                        <img className='client-image' src={clientMeet}/>
-                        <img className='client-image' src={clientMarker}/>
+
+    const isWide = useMediaQuery({
+        query: '(min-width: 1023px)'
+      })
+    return (
+        <>  
+            <div className='main-page-container'>
+                <div className='main-page-text'>
+                    <h1>Make remote work</h1>
+                    <p>Get your team in sync, no matter your location. <br /><span>Streamline processes. create team rituals, and watch productivity soar.</span></p>
+                    
+                    <button className='learn-more-button'>
+                        Learn more
+                    </button>
+                    <div>
+                        <div className='client-image-wrapper'>
+                            <img className='client-image' src={clientDatabiz}/>
+                            <img className='client-image' src={clientAudiophile}/>
+                            <img className='client-image' src={clientMeet}/>
+                            <img className='client-image' src={clientMarker}/>
+                        </div>
                     </div>
                 </div>
+                <img className='main-page-image' src={isWide ? heroImageDesktop : heroImageMobile }/>
             </div>
-            <img className='main-page-image' src={heroImageDesktop}/>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default MainPage
